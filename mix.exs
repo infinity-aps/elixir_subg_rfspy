c2b728fb00e9172573d452d4441f1a48b9239564
Tim Mecklem <timothy@mecklem.com>
2017-09-10 18:52:50 -0400

    Make it a hex package

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule SubgRfspy.Mixfile do
   def project do
     [
       app: :subg_rfspy,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: "0.9.0",
+      elixir: ">= 1.4.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "SubgRfspy",
+      source_url: "https://github.com/infinity-aps/elixir_subg_rfspy",
+      description: description(),
+      package: package()
     ]
   end
 
@@ -19,5 +23,19 @@ defmodule SubgRfspy.Mixfile do
     [{:nerves_uart, "~> 0.1.1"},
      {:csv, "~> 2.0.0"},
      {:credo, "~> 0.8", only: [:dev, :test], runtime: false}]
+  end
+
+  defp description do
+    """
+    SubgRfspy is a library to handle sub-GHz wireless packet communication via a TI cc1110 chip running subg_rfspy.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Timothy Mecklem"],
+      licenses: ["MIT License"],
+      links: %{"Github" => "https://github.com/infinity-aps/elixir_subg_rfspy"}
+    ]
   end
 end
